e29e7d7bb4eb709cd2b8855a63a07cdc39044dd3
fulong98 <fulong98@hotmail.com>
2019-05-24 17:33:56 +0800

    votingSystem

--- votingResult.php
+++ votingResult.php
@@ -36,14 +36,13 @@ try {
                 }
                 //var_dump($products);
                 
-                $vals = array_count_values($products);// calculate the number of occurerance 
-                
-                //var_dump($vals);
+                $vals = array_count_values($products);// calculate the number of occurerance               
                 $keys = array_keys($vals); // get the key
                 // var_dump($vals);
                 
                 $bestParticipant='';
                 echo "<div style='text-align:center;width:300px;margin:50px auto;'>";
+                echo "<h3>投票票数</h3>";
                  for($i=0;$i<count($vals);$i++)
                  {
                     
@@ -65,12 +64,16 @@ try{
         if($stmt->execute()){
             $row1 = $stmt->fetch(PDO::FETCH_ASSOC);
             $score1[] = $row1;
+            $negative1=$score1[0]['lilun']+$score1[0]['zhixun_1']+$score1[0]['yuyan_1']+$score1[0]['ziyou_1'];
+            $negative2=$score1[0]['bolun']+$score1[0]['gongbian']+$score1[0]['yuyan_2']+$score1[0]['ziyou_2'];
+            $negative3=$score1[0]['zhixun_3']+$score1[0]['xiaojie']+$score1[0]['yuyan_3']+$score1[0]['ziyou_3'];
+            $negetive4=$score1[0]['chenci']+$score1[0]['yuyan_4']+$score1[0]['ziyou_4'];
             echo "<div class='row' style='text-align:center;width:300px;margin:auto auto;'><div class='col-sm-6'>";
             echo "总分";
-            echo "<br>反方一辩".($score1[0]['lilun']+$score1[0]['zhixun_1']+$score1[0]['yuyan_1']+$score1[0]['ziyou_1']);
-            echo "<br>反方二辩".($score1[0]['bolun']+$score1[0]['gongbian']+$score1[0]['yuyan_2']+$score1[0]['ziyou_2']);
-            echo "<br>反方三辩".($score1[0]['zhixun_3']+$score1[0]['xiaojie']+$score1[0]['yuyan_3']+$score1[0]['ziyou_3']);
-            echo "<br>反方四辩".($score1[0]['chenci']+$score1[0]['yuyan_4']+$score1[0]['ziyou_4']);
+            echo "<br>反方一辩".($negative1);
+            echo "<br>反方二辩".($negative2);
+            echo "<br>反方三辩".($negative3);
+            echo "<br>反方四辩".($negetive4);
             echo "</div>";
         }
         $stmt1 = $conn->prepare("SELECT * FROM `competition` WHERE judge_id='$userID' and competition_id='$competition_id1' and side=1");
@@ -81,12 +84,88 @@ try{
             $score[] = $row1;
             echo "<div class='col-sm-6'>";
             echo "总分";
-            echo "<br>正方一辩".($score[0]['lilun']+$score[0]['zhixun_1']+$score[0]['yuyan_1']+$score[0]['ziyou_1']);
-            echo "<br>正方二辩".($score[0]['bolun']+$score[0]['gongbian']+$score[0]['yuyan_2']+$score[0]['ziyou_2']);
-            echo "<br>正方三辩".($score[0]['zhixun_3']+$score[0]['xiaojie']+$score[0]['yuyan_3']+$score[0]['ziyou_3']);
-            echo "<br>正方四辩".($score[0]['chenci']+$score[0]['yuyan_4']+$score[0]['ziyou_4']);
+            $affirmative1=$score[0]['lilun']+$score[0]['zhixun_1']+$score[0]['yuyan_1']+$score[0]['ziyou_1'];
+            $affirmative2=$score[0]['bolun']+$score[0]['gongbian']+$score[0]['yuyan_2']+$score[0]['ziyou_2'];
+            $affirmative3=$score[0]['zhixun_3']+$score[0]['xiaojie']+$score[0]['yuyan_3']+$score[0]['ziyou_3'];
+            $affirmative4=$score[0]['chenci']+$score[0]['yuyan_4']+$score[0]['ziyou_4'];
+            echo "<br>正方一辩".($affirmative1);
+            echo "<br>正方二辩".($affirmative2);
+            echo "<br>正方三辩".($affirmative3);
+            echo "<br>正方四辩".($affirmative4);
             echo "</div></div>";
         }
+        $participant_score=[];   // use to store key=>value  ...... value = TotalVotingTicketbyParticipant*100+his competition result
+        $ranking=[]; // use to score user value of array :participant_score  for ranking
+        for($j=0;$j<sizeof($vals);$j++)
+        {
+            // echo $keys[$j].$vals[$keys[$j]];
+            if($keys[$j]=="正方一辩")
+            {
+                array_push($participant_score,(array($keys[$j] => $vals[$keys[$j]]*100+$affirmative1)));
+                array_push($ranking,$vals[$keys[$j]]*100+$affirmative1);
+            
+            }
+            if($keys[$j]=="正方二辩")
+            {array_push($participant_score,(array($keys[$j] => $vals[$keys[$j]]*100+$affirmative2)));
+                array_push($ranking,$vals[$keys[$j]]*100+$affirmative2);
+            }
+            if($keys[$j]=="正方三辩")
+            {
+                array_push($participant_score,(array($keys[$j] => $vals[$keys[$j]]*100+$affirmative3)));
+                array_push($ranking,$vals[$keys[$j]]*100+$affirmative3);
+                
+            }
+            if($keys[$j]=="正方四辩")
+            {
+                array_push($participant_score,(array($keys[$j] => $vals[$keys[$j]]*100+$affirmative4)));
+                array_push($ranking,$vals[$keys[$j]]*100+$affirmative4);
+            }
+            if($keys[$j]=="反方一辩")
+            {
+                array_push($participant_score,(array($keys[$j] => $vals[$keys[$j]]*100+$negative1)));
+                array_push($ranking,$vals[$keys[$j]]*100+$negative1);
+            }
+            if($keys[$j]=="反方二辩")
+            {
+                array_push($participant_score,(array($keys[$j] => $vals[$keys[$j]]*100+$negative2)));
+                array_push($ranking,$vals[$keys[$j]]*100+$negative2);
+            }
+            if($keys[$j]=="反方三辩")
+            {
+                array_push($participant_score,(array($keys[$j] => $vals[$keys[$j]]*100+$negative3)));
+                array_push($ranking,$vals[$keys[$j]]*100+$negative3);
+            }
+            if($keys[$j]=="反方四辩")
+            {
+                array_push($participant_score,(array($keys[$j] => $vals[$keys[$j]]*100+$negative4)));
+                array_push($ranking,$vals[$keys[$j]]*100+$negative4);
+            }
+        }
+        rsort($ranking);
+        // var_dump($ranking);
+        // var_dumP($participant_score);
+        // echo $participant_score[0][$keys[0]];
+        $bestParticipant=[];
+        
+        for($i=0;$i<3 && $i<$j;$i++)
+        {
+            for($j=0;$j<sizeof($participant_score);$j++)
+            {
+                if($participant_score[$j][$keys[$j]]==$ranking[$i]){array_push($bestParticipant,key($participant_score[$j])); }
+            }
+        }
+        echo "<div style='width:300px;margin:30px auto;text-align:center;'><h3>最佳三位辩手</h3>";
+        echo $bestParticipant[0]."<br>";
+        echo $bestParticipant[1]."<br>";
+        if(sizeof($bestParticipant)>2){
+        echo $bestParticipant[2]."<br>";}
+        echo "</div>";
+        echo "<div style='width:300px;margin:30px auto;text-align:center;'><h3>最佳辩手</h3>";
+        echo $bestParticipant[0]."<br>";        
+        echo "</div>";
+
+        
+        // var_dump($participant_score);
             
             
             // 
