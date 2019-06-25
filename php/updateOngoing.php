6ac785308faecfdb8b0e93fb1bfa376ecf4a883b
jiale <tanjiale1029@gmail.com>
2019-06-25 16:29:56 +0800

    debugging

--- php/updateOngoing.php
+++ php/updateOngoing.php
@@ -3,25 +3,30 @@
 
   $competitionID;
 
-  if(!empty($_POST['competitionID'])){
-    $competitionID = $_POST['competitionID'];
-    if(strcmp($_POST['submit'], 'Deactivate') == 0){
-      foreach($competitionID as $competition){
-        $sql = $conn->prepare("UPDATE titles SET available = 0 WHERE competition_id = ?");
-        $sql->execute([$competition]);
+  try{
+    if(!empty($_POST['competitionID'])){
+      $competitionID = $_POST['competitionID'];
+      if(strcmp($_POST['submit'], 'Deactivate') == 0){
+        foreach($competitionID as $competition){
+          $sql = $conn->prepare("UPDATE titles SET available = 0 WHERE competition_id = ?");
+          $sql->execute([$competition]);
+        }
+        echo 'Deactivation successful!';
+      }else{
+        foreach($competitionID as $competition){
+          $sql = $conn->prepare("UPDATE titles SET available = 1 WHERE competition_id = ?");
+          $sql->execute([$competition]);
+        }
+        echo 'Activation successful!';
       }
-      echo 'Deactivation successful!';
+      header("Location: ../dashboard.php"); 
+      
     }else{
-      foreach($competitionID as $competition){
-        $sql = $conn->prepare("UPDATE titles SET available = 1 WHERE competition_id = ?");
-        $sql->execute([$competition]);
-      }
-      echo 'Activation successful!';
+      echo 'There are some issues with the server, please try again or contact admin.';
     }
-    header("Location: ../dashboard.php"); 
-    
-  }else{
-    echo 'There are some issues with the server, please try again or contact admin.';
+  }catch(PDOException $e){
+    echo "Error: ".$e->getMessage();
   }
+  
 
 ?>
